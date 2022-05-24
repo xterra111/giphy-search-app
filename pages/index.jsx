eb57c012d26051eb13c1dddd14efeba7a026fc56
Harish Sathisan <harish.sathisan@icloud.com>
2022-05-24 16:51:32 -0700

    fixing issues.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
+import { useEffect } from "react";
 
 
+export default function Home (initialData) {
+	
+	useEffect( () => {
+		console.log( initialData );
+	});
 
-export default function Home() {
 	return (
 		<div className='container'>
 			<Head>
@@ -13,6 +18,31 @@ export default function Home() {
 
 			</Head>
 			<h1>Giphy App</h1>
+			<div className='results-grid'>
+			{initialData.trendingGifsFetched.data.map( ( item, index ) => {
+				return (
+					<div key={index}>
+						<h3>{item.title}</h3>
+						<img src={item.images.original.url} />
+						{/* Need to research is image is better for NextJS */}
+					</div>
+
+				)
+			} )}
+				</div>
+		
+
 		</div>
-	);
+	)
+}
+
+export async function getStaticProps() {
+	const trendingGifs = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=XXX&limit=5')
+	 const trendingGifsFetched = await trendingGifs.json();
+	
+	return {
+		props: {
+			trendingGifsFetched: trendingGifsFetched
+		}
+	}
 }
