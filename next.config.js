477300a5580feab6c9a41c64ad33f4d18759b9a6
Harish Sathisan <harish.sathisan@icloud.com>
2022-05-23 19:40:12 -0700

    First Changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+//module.exports = nextConfig;
+
+module.exports = {
+	devIndicators: {
+		autoPrerender: false,
+	},
+	//nextConfig,
+};
